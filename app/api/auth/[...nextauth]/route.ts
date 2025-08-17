9e02b788f0c28533ba3e3b5019221605175789f4
Sudhanshu <sudhanshu007@bhu.ac.in>
2025-08-17 17:54:10 +0530

    fix typescript error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,6 @@
 
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import clientPromise from '@/lib/db';
-import type { Session, User } from 'next-auth';
-import type { AdapterUser } from 'next-auth/adapters';
-
-export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise, { databaseName: process.env.DATABASE_NAME }),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    session: ({ session, user }: { session: Session; user: User | AdapterUser }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: (user as User).id,
-      },
-    }),
-  },
-};
+import { authOptions } from '@/lib/auth';
 
 const handler = NextAuth(authOptions);
 
